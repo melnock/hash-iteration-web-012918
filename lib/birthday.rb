78176f35a25e2d288455ae16c406887df46bf0fe
Melissa Nock <melnock@gmail.com>
2018-01-19 14:44:04 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each{|birthday_kids, age|
+  birthday_kids.each{|kids_name, age|
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"}
 end
 
